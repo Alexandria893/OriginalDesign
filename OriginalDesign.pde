08616a1e93c02f1f07ccd268ebc2d808bc051323
unknown <albrown1@697-334-Aldburg.sf.k12.internal>
2015-08-28 10:21:31 -0700

    Added two ellipse

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,16 +1,16 @@
-
+int diameter= 10;
 void setup()
 {
 size(500,500);
 //fill(0); //text color
 textSize(32);
-noLoop();
 }
 
 void draw()
 {
 	background(150);
 	DrawEllipse();
+	DrawEllipse2();
 	keyPressed();
 
 
@@ -19,13 +19,37 @@ void draw()
 void keyPressed()
 {
 fill(0,0,0);
+//text(key,250,250);
 text(key,250,250);
 redraw();
 }
 
-void DrawEllipse(){
-fill(random(0,255),random(0,255),random(0,255),100);
+void DrawEllipse2()
+{
+fill(random(0,255),random(0,255),random(0,255));
 stroke(random(0,255),random(0,255),random(0,255));
-ellipse(250,250,random(0,500),random(0,500));
+ellipse(250,250,diameter,diameter);
+frameRate(10);
+if (diameter<250)
+{
+diameter=diameter+20;
+if (diameter>=250)
+	{
+		diameter=10;
+	}
+}
+	
 }
 
+void DrawEllipse()
+{
+diameter=450;
+fill(random(0,255),random(0,255),random(0,255));
+stroke(random(0,255),random(0,255),random(0,255));
+ellipse(250,250,diameter,diameter);
+frameRate(10);
+while (diameter>=150)
+{
+diameter=diameter-20;
+}
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,19 +14,19 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-
+int diameter= 10;
 public void setup()
 {
 size(500,500);
 //fill(0); //text color
 textSize(32);
-noLoop();
 }
 
 public void draw()
 {
 	background(150);
 	DrawEllipse();
+	DrawEllipse2();
 	keyPressed();
 
 
@@ -35,16 +35,40 @@ public void draw()
 public void keyPressed()
 {
 fill(0,0,0);
+//text(key,250,250);
 text(key,250,250);
 redraw();
 }
 
-public void DrawEllipse(){
-fill(random(0,255),random(0,255),random(0,255),100);
+public void DrawEllipse2()
+{
+fill(random(0,255),random(0,255),random(0,255));
 stroke(random(0,255),random(0,255),random(0,255));
-ellipse(250,250,random(0,500),random(0,500));
+ellipse(250,250,diameter,diameter);
+frameRate(10);
+if (diameter<250)
+{
+diameter=diameter+20;
+if (diameter>=250)
+	{
+		diameter=10;
+	}
+}
+	
 }
 
+public void DrawEllipse()
+{
+diameter=450;
+fill(random(0,255),random(0,255),random(0,255));
+stroke(random(0,255),random(0,255),random(0,255));
+ellipse(250,250,diameter,diameter);
+frameRate(10);
+while (diameter>=150)
+{
+diameter=diameter-20;
+}
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
